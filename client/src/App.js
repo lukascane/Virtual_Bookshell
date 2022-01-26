b005af71e2438f88c5421a486d91238efb611b7d
Riccardo Limiti <riccardo.limiti@gmail.com>
2022-01-26 11:19:16 +0100

    some NavBar idea

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import Footer from './components/Footer/Footer';
+import NavBarLoggedIn from './components/Navbar/NavBarLoggedIn';
+import NavbarGuest from './components/Navbar/NavBarGuest';
+
 
 function App() {
   return (
     <div className="App">
-      <Footer />
+
     </div>
   );
 }
