0e43021e93a2d11a10fdcc30c834c9f9db58adc1
Lukasz Cane <lukastrz6@gmail.com>
2022-01-26 11:22:56 +0100

    cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import Footer from './components/Footer/Footer';
 
+import Card from './components/Card/Card'
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <Card />
     </div>
   );
 }
