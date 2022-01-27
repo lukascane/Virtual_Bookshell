5fb898a745443991dc888d4df942aa5c4d4edc14
Lukasz Cane <lukastrz6@gmail.com>
2022-01-27 14:02:11 +0100

    cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-<<<<<<< HEAD
 import Card from './components/Card/Card';
-=======
 import Footer from './components/Footer/Footer';
 import NavBarLoggedIn from './components/Navbar/NavBarLoggedIn';
 import NavbarGuest from './components/Navbar/NavBarGuest';
 import HomeBanner from './components/HomeBanner/HomeBanner';
->>>>>>> 4b2626a0616c33fb1f6d0d3b421b3621c9405393
+import MiniCard from './components/Card/MiniCard';
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Card />
-=======
+      {/* <Card /> */}
+      <MiniCard />
+      {/* <HomeBanner /> */}
     
-    <HomeBanner />
-    
->>>>>>> 4b2626a0616c33fb1f6d0d3b421b3621c9405393
     </div>
   );
 }
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from 'react';
 
-export default function Card() {
-  return (
-<div id="bigcard" className="card" style={{width: "18rem"}}>
-<div>
-  <img src="https://covers.openlibrary.org/b/id/8166951-M.jpg
-" class="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="button btn btn-primary">Go somewhere</a>
-    <a href="#" className="button btn btn-primary">Go somewhere</a>
-    <a href="#" className="button btn btn-primary">Go somewhere</a>
-    </div> 
+function Card() {
+  return <div id="bigCard">
+  <div class="row d-flex justify-content-between ">
+  <div className='container mb-5'>
+  <div  className="shadow-lg row card" style={{width: "18rem"}}>
+  <div className="photoframe card-img-top">
+    <img src="https://covers.openlibrary.org/b/id/8166951-M.jpg
+  " className="card-img-top mt-3" alt="..."/>
+  </div>
+    <div className="card-body text-left">
+      <h5 className="card-title">Book Title</h5>
+      <p className="card-text">Some quick example </p>
+      </div>
+      
+      <div className="button">
+      <a href="#" className="btn cardBtn homeBtn btn-sm">List to read</a>
+      <a href="#" className="btn cardBtn homeBtn btn-sm">Review</a>
+      <a href="#" className="btn cardBtn homeBtn btn-sm">Read/+Collection</a>
+      </div>
+  </div>;
 </div>
 </div>
-  )
-};
+</div>;
+}
+
+export default Card;
