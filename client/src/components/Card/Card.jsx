fedccaf593d2dba5348fb96b534b63fb5ad5d6db
Lukasz Cane <lukastrz6@gmail.com>
2022-01-27 10:26:53 +0100

    updates about cards

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 export default function Card() {
   return (
-<div className="card" style={{width: "18rem"}}>
-  <img src="..." class="card-img-top" alt="..."/>
+<div id="bigcard" className="card" style={{width: "18rem"}}>
+<div>
+  <img src="https://covers.openlibrary.org/b/id/8166951-M.jpg
+" class="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
+    <a href="#" className="button btn btn-primary">Go somewhere</a>
+    <a href="#" className="button btn btn-primary">Go somewhere</a>
+    <a href="#" className="button btn btn-primary">Go somewhere</a>
+    </div> 
+</div>
 </div>
   )
 };
