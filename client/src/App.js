ab16a2df6774e16f6a1ebaead4a952ff0f2b562c
Peony Langner <peony.langner@hotmail.de>
2022-01-31 10:22:22 +0100

    Adjusted app.js from main

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,16 @@ import Footer from './components/Footer/Footer';
 import NavBarLoggedIn from './components/Navbar/NavBarLoggedIn';
 import NavbarGuest from './components/Navbar/NavBarGuest';
 import HomeBanner from './components/HomeBanner/HomeBanner';
+import SearchForm from './components/Form/SearchForm';
 import MiniCard from './components/Card/MiniCard';
-import axios from './util/axiosInstance';
-import { useState, useEffect } from 'react';
-import search from './services/bookApi/searchTitleAuthApi';
-import sendBookInfoApi from './services/bookApi/sendBookInfoApi';
 
 function App() {
-  useEffect(() => {
-    search();
-  }, []);
   return (
     <div className="App">
+      <SearchForm />
       {/* <Card /> */}
-      {/* <MiniCard /> */}
+      <MiniCard />
       {/* <HomeBanner /> */}
-      <button onClick={sendBookInfoApi}>Click to read</button>
     </div>
   );
 }
