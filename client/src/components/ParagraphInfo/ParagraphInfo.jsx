053dcb61addf077912a92555da363c0301a0126d
Lukasz Cane <lukastrz6@gmail.com>
2022-02-03 14:34:17 +0100

    paragrapfINfo created

---
+++ client/src/components/ParagraphInfo/ParagraphInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState, useContext } from 'react';
+
+
+export default function ParagraphInfo() {
+ 
+
+  return (
+    <div id="searchForm">
+      <div className="container formWrapper">
+        
+      <div className="form-text mt-3 vText">
+        You found 11111 books text to be a variable of search.
+      </div>
+    </div>
+    </div>
+  );
+}
