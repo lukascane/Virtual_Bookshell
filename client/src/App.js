b7e1fb196f984fd51c282de2b906efd9848ea706
Riccardo Limiti <riccardo.limiti@gmail.com>
2022-02-03 11:39:40 +0100

    landingInProgress

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,9 @@
-import Card from './components/Card/Card';
-import Footer from './components/Footer/Footer';
-import NavBarLoggedIn from './components/Navbar/NavBarLoggedIn';
-import NavbarGuest from './components/Navbar/NavBarGuest';
-import HomeBanner from './components/HomeBanner/HomeBanner';
-import SearchForm from './components/Form/SearchForm';
-import MiniCard from './components/Card/MiniCard';
-import BookModal from './components/Modals/BookModal';
-import ReviewModal from './components/Modals/ReviewModal';
-import LoginForm from './components/Form/LoginForm';
-import SignUpForm from './components/Form/SignUpForm';
-import EditProfileForm from './components/ProfileComponents/EditProfileForm';
-import ProfileCard from './components/ProfileComponents/ProfileCard';
-import ErrorModal from './components/Modals/ErrorModal';
-import ListCard from './components/Card/ListCard';
-import ListCardToRead from './components/Card/ListCardToRead';
+import MainRouter from './hoc/MainRouter';
 
 function App() {
   return (
     <div className="App">
-    
-    <ErrorModal />
-
+    <MainRouter />
     </div>
   );
 }
--- client/src/hoc/MainRouter.jsx
+++ client/src/hoc/MainRouter.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter
+} from "react-router-dom";
+import Footer from '../components/Footer/Footer'
+import NavG from '../components/Navbar/NavBarGuest'
+import Landing from '../pages/Landing/Landing';
+
 
 function MainRouter() {
-  return <div>
-      <Landing />
+  return ( 
+  <BrowserRouter>
+  <NavG />
+  <main>
+  <Landing />
       
-  </div>;
+  
+  </main>
+  <Footer />
+  </BrowserRouter>
+  );
 }
 
 export default MainRouter;
