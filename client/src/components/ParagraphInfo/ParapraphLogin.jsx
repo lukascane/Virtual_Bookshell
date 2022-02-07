4481c2146816fa0ec7ee76be8560f4c282fa6453
Lukasz Cane <lukastrz6@gmail.com>
2022-02-07 13:34:48 +0100

    Parapgraphs 2x

---
+++ client/src/components/ParagraphInfo/ParapraphLogin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ParapraphLogin() {
+    return (
+        <div id="searchForm">
+          <div className="container formWrapper">
+            
+          <div className="form-text mt-3 vText">
+            Please correct you password /username is wrong.
+          </div>
+        </div>
+        </div>
+      );
+    }
+export default ParapraphLogin;
---
+++ client/src/components/ParagraphInfo/PrapraphRegister.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function PrapraphRegister() {
+    return (
+        <div id="searchForm">
+          <div className="container formWrapper">
+            
+          <div className="form-text mt-3 vText">
+            Please make sure that all fields are filled.
+          </div>
+        </div>
+        </div>
+      );
+}
+
+export default PrapraphRegister;
