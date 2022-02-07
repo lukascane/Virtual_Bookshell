13e02164404171c010d47e8e623b47655c94f7cd
Lukasz Cane <lukastrz6@gmail.com>
2022-02-07 08:38:00 +0100

    loginview

--- client/src/hoc/MainRouter.jsx
+++ client/src/hoc/MainRouter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import BooksProvider from '../context/booksContextApi/BooksProvider';
 import Profile from '../pages/Profile/Profile'
-
+import LoginForm from '../components/Form/LoginForm';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,6 +13,7 @@ import Footer from '../components/Footer/Footer';
 import NavG from '../components/Navbar/NavBarGuest';
 import Landing from '../pages/Landing/Landing';
 import ParagraphInfo from '../components/ParagraphInfo/ParagraphInfo';
+import LoginForm from '../components/Form/LoginForm';
 
 
 function MainRouter() {
@@ -21,7 +22,7 @@ function MainRouter() {
       <BrowserRouter>
         <NavG />
         <main>
-
+<LoginForm />
         </main>
         <Footer />
       </BrowserRouter>
---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from '../../components/Form/LoginForm';
+
+function Login() {
+  return <div id="loginview" className="container">
+      <div>
+<LoginForm />
+    </div>
+  </div>;
+}
+
+export default Login;
