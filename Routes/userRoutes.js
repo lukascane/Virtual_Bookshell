33777861b2f93040e6e93bae67f0b25242bcdc9d
Peony Langner <peony.langner@hotmail.de>
2022-02-07 16:11:32 +0100

    Checked first time userAuth

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./../Controllers/userControllers');
-const passport = require("passport");
+const passport = require('passport');
 
 router.post('/register', controller.registerUser);
 
 // everything from here lower is protected
-
-router.use(passport.authenticate("jwt", { session: false }));
+router.use(passport.authenticate('jwt', { session: false }));
 router.get('/list', controller.listUsers);
 
 module.exports = router;
-
