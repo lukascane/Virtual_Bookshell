02b385f26568f34741981df0d51fb049d0dc6dfd
Peony Langner <peony.langner@hotmail.de>
2022-02-08 15:28:50 +0100

    Connected registration & login to database

--- client/src/components/Form/LoginForm.jsx
+++ client/src/components/Form/LoginForm.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
+import axios from '../../util/axiosInstance';
 
 export default function LoginForm() {
-  return <div id='login' className='d-flex justify-content-center row align-items-center'>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log('Login the user');
+    const formData = new FormData(event.target);
 
-    <h4 className='mt-2 mb-5'>Login with your existing account to start</h4>
-    <h4 className='mt-2'>Username:</h4>
-    <input type="text" id="user_name_input" className="form-control w-100 mx-1 text-light mb-5"/>
-    <h4 className='mt-2'>Password:</h4>
-    <input type="text" id="password_input" className="form-control w-100 mx-1 text-light mb-5"/>
-    <div>
-    <button className='rounded-pill buttons' type='button'>LOGIN</button>
-    </div>
+    const data = {
+      username: formData.get('username'),
+      password: formData.get('password'),
+    };
+    console.log(data);
+    try {
+      const response = await axios.post('/api/user/login', data);
 
-  </div>;
+      if (response.status === 200) {
+        //everything went well!
+        console.log('You are now logged in');
+      }
+    } catch (error) {
+      console.log('The error is: ', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div
+        id="login"
+        className="d-flex justify-content-center row align-items-center"
+      >
+        <h4 className="mt-2 mb-5">Login with your existing account to start</h4>
+        <h4 className="mt-2">Username:</h4>
+        <input
+          type="text"
+          name="username"
+          id="user_name_input"
+          className="form-control w-100 mx-1 text-light mb-5"
+        />
+        <h4 className="mt-2">Password:</h4>
+        <input
+          type="password"
+          name="password"
+          id="password_input"
+          className="form-control w-100 mx-1 text-light mb-5"
+        />
+        <div>
+          <button className="rounded-pill buttons">LOGIN</button>
+        </div>
+      </div>
+    </form>
+  );
 }
