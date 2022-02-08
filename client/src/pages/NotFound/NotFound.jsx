c8ec1e81094b3bc0bec99ab68087cce663aa71c4
Lukasz Cane <lukastrz6@gmail.com>
2022-02-08 10:41:05 +0100

    notfound

---
+++ client/src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NotFound from '../../components/Modals/ErrorModal';
+
+function NotFound() {
+    return <div id="errorview">
+    <div className="container" style={{marginTop:"130px"}}>
+    <NotFound />
+  </div>
+</div>;
+}
+
+export default NotFound;
