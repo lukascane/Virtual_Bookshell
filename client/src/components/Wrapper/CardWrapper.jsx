47e2a844df643aed84a32323a189c2987ef92ef2
Peony Langner <peony.langner@hotmail.de>
2022-02-09 11:32:18 +0100

    added useEffect on search

--- client/src/components/Wrapper/CardWrapper.jsx
+++ client/src/components/Wrapper/CardWrapper.jsx
@@ -1,27 +1,20 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import BookContext from '../../context/booksContextApi/BookContext';
 import Card from '../Card/Card';
-import {Carousel} from '3d-react-carousal';
+import { Carousel } from '3d-react-carousal';
 
-const slides = [
-    <Card />,
-    <Card />,
-    <Card />,
-    <Card />,
-    <Card />,
-
-    ];
+const slides = [<Card />, <Card />, <Card />, <Card />, <Card />];
 
 export default function CardWrapper() {
-/*   const { onClickFetchData, bookInfos } = useContext(BookContext);
+  /*   const { onClickFetchData, bookInfos } = useContext(BookContext);
   console.log(bookInfos); */
 
   return (
-    <div className='' id='carousel'>
-    <Carousel slides={slides} autoplay={false} interval={1000} />
+    <div className="" id="carousel">
+      <Carousel slides={slides} autoplay={false} interval={1000} />
     </div>
 
-/*     <div className='d-flex justify-content-evenly col'>
+    /*     <div className='d-flex justify-content-evenly col'>
       {bookInfos.map((item, index) => (
         <Card item={item} id={index}></Card>
       ))}
