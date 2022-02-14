8d82eb1876f6b2f3b055ecee62f521624b4f59bf
Riccardo Limiti <riccardo.limiti@gmail.com>
2022-02-14 15:38:49 +0100

    monadyIsMyDay

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import EditProfileForm from '../../components/ProfileComponents/EditProfileForm'
 import ProfileCard from '../../components/ProfileComponents/ProfileCard';
 
 function Profile() {
-  return <div id="profileview" className="container">
+  return <div id="profileView" className="container mb-5">
       <div className="d-flex col justify-content-center profileviewcontainer mb-5" style={{marginTop:"130px"}}>
       <ProfileCard />
       <EditProfileForm />
