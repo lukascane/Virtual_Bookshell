3a2ba45a0e2dec60ced047c0c14dcf0df8459b73
Peony Langner <peony.langner@hotmail.de>
2022-02-15 11:29:05 +0100

    Added numFound to searchTitleAuthApi

--- client/src/services/bookApi/searchTitleAuthApi.js
+++ client/src/services/bookApi/searchTitleAuthApi.js
@@ -1,42 +1,37 @@
 import searchAxiosInstance from '../../util/searchAxiosInstance';
 
-
 function searchTitleAuthApi(search, searchCathegory) {
- let config = null
-/*  {
+  let config = null;
+  /*  {
     params: {
       q: 'the+lord+of+the+rings', 
       limit: 8,
     }, 
   }; */
-  if (searchCathegory == "Title") { 
-  config = {
-    params: {
-      q: search, 
-      limit: 8,
-    }, 
-  };
-
-  };
-  if (searchCathegory == "Year") { 
+  if (searchCathegory == 'Title') {
     config = {
       params: {
-        q: search, 
+        q: search,
         limit: 8,
-      }, 
-
-  };
-
-};
-  if (searchCathegory == "Author") { 
-    config = {
-      params: { 
-       author: search,
-       limit: 8,
-      }, 
-    
+      },
+    };
   }
-};
+  if (searchCathegory == 'Year') {
+    config = {
+      params: {
+        q: search,
+        limit: 8,
+      },
+    };
+  }
+  if (searchCathegory == 'Author') {
+    config = {
+      params: {
+        author: search,
+        limit: 8,
+      },
+    };
+  }
   return searchAxiosInstance
     .get('/', config)
     .then((response) => {
@@ -49,10 +44,10 @@ function searchTitleAuthApi(search, searchCathegory) {
           cover_i: item.cover_i,
           isbn: item.isbn,
           pages: item.number_of_pages_median,
+          numFound: data.numFound,
         };
       });
 
-      
       return showData;
     })
     .catch((err) => {
