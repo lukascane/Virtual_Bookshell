76bdd2080ad357938a3b4ac7516c513d3b1e45c9
Riccardo Limiti <riccardo.limiti@gmail.com>
2022-02-21 10:18:07 +0100

    littleBug

--- client/src/components/Card/ListCardToRead.jsx
+++ client/src/components/Card/ListCardToRead.jsx
@@ -52,24 +52,28 @@ export default function ListCardToRead({ item }) {
           )}
         </a>
         <div className="bookInfo2">
-          <div className="ms-3 mt-4 bigHide">
+          <div className="ms-4 mt-4 bigHide">
             <p className="mt-3 bttl">{newTitle}</p>
             <p>{item.author}</p>
             <p>Pages: {item.pages}</p>
             <p>more variable</p>
-            <button onClick={() => onClickShowAlert(3)}className="btn btLink rmv" style={{width:'120px'}}>REMOVE</button>
+            <button onClick={() => onClickShowAlert(3)}className="btn btLink rmv d-flex justify-content-between align-items-center" style={{width:'140px'}}>REMOVE<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+</svg></button>
           </div>
         </div>
       </div>
 
       <div className="dataBookCont d-flex justify-content-evenly col">
         <div className="bookInfo">
-          <div className="ms-3 mt-4">
+          <div className="ms-4 mt-4">
             <p className="mt-3 bttl">{newTitle}</p>
             <p>{item.author}</p>
             <p>Pages: {item.pages}</p>
             <p>more variable</p>
-            <button onClick={() => onClickShowAlert(3)} className="btn btLink2" style={{margin:'0px'}}>REMOVE</button>
+            <button onClick={() => onClickShowAlert(3)} className="btn btLink2 d-flex justify-content-between align-items-center" style={{margin:'0px', width:'170px'}}>REMOVE<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+  <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
+</svg></button>
           </div>
         </div>
 
