5869936c4b7bd8fe32fb9d96af7ea7c41dd58dd9
Riccardo Limiti <riccardo.limiti@gmail.com>
2022-02-23 15:28:44 +0100

    fixDescriptionAPI

--- client/src/components/Description/Description.jsx
+++ client/src/components/Description/Description.jsx
@@ -11,16 +11,19 @@ function Description({ item }) {
     return (
       descriptionAxiosInstance
         .get(`${item}.json`)
-        // .get('/works/OL45883W.json')
         .then((response) => {
           const data = response.data;
-
-          if (data.description !== undefined) {
-            setDescription(data.description);
-          } else if (data.description === {}) {
-            return setDescription('No text here');
+          console.log(data,'im data')
+          if(data.description !== null){
+            if(typeof data.description === 'object'){
+              setDescription(data.description.value)
+            }else if (data.description) {
+              setDescription(data.description)
+            }else {
+              return setDescription('NO DESCRIPTION TEXT: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat consequat arcu sit amet rhoncus. Sed ligula augue, efficitur ut varius a, faucibus vel lacus. Ut congue magna quis massa luctus scelerisque.')
+            }
           } else {
-            return setDescription('NO DESCRIPTION TEXT: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat consequat arcu sit amet rhoncus. Sed ligula augue, efficitur ut varius a, faucibus vel lacus. Ut congue magna quis massa luctus scelerisque.');
+            return setDescription('NO DESCRIPTION TEXT: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris volutpat consequat arcu sit amet rhoncus. Sed ligula augue, efficitur ut varius a, faucibus vel lacus. Ut congue magna quis massa luctus scelerisque.')
           }
         })
         .catch((err) => {
