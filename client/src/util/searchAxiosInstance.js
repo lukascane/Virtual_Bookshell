b6d7446104a7238508c25957f7ede6aac24e079a
Peony Langner <peony.langner@hotmail.de>
2022-02-24 13:40:19 +0100

    Prepared for deploy

--- client/src/util/searchAxiosInstance.js
+++ client/src/util/searchAxiosInstance.js
@@ -3,6 +3,5 @@ import axios from 'axios';
 const searchAxiosInstance = axios.create();
 
 searchAxiosInstance.defaults.baseURL = 'https://openlibrary.org/search.json?';
-// searchAxiosInstance.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export default searchAxiosInstance;
